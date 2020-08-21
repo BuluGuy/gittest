9fcafbae6322eaef83418f52b3cddc6fb8d80682
BuluGuy <BLGY2000@outlook.com>
2020-08-21 18:46:19 +0800

    测试git push

---
+++ gittest/src/test/java/com/nuist/gittest/GittestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuist.gittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
